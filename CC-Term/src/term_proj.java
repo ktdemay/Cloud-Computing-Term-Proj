d889c16fe9602c6b4137e44aa7f893d5e0843284
Kevin DeMaioribus <ktdemay@comcast.net>
2020-11-21 20:35:01 -0500

    Basic GUI template

--- CC-Term/src/term_proj.java
+++ CC-Term/src/term_proj.java
@@ -2,20 +2,8 @@ import javax.swing.*;
 
 public class term_proj {
     private JPanel panel1;
-    private JButton rstudioB;
-    private JButton spyderB;
-    private JButton ibmB;
-    private JButton gitB;
-    private JButton orangeB;
-    private JButton vsB;
-    private JButton hadoopB;
-    private JButton sparkB;
-    private JButton tableauB;
-    private JButton ssB;
-    private JButton tensorB;
-    private JButton mdB;
-    private JButton npB;
-    private JButton jupyterB;
+    private JButton searchB;
+    private JButton topB;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("term_proj");
