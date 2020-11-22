ce61c5a577b702d4d40cfd8bd65a4342cc9723c1
Kevin DeMaioribus <ktdemay@comcast.net>
2020-11-21 20:50:06 -0500

    added buttons

--- CC-Term/src/term_proj.java
+++ CC-Term/src/term_proj.java
@@ -1,9 +1,20 @@
 import javax.swing.*;
 
-public class term_proj {
+public class term_proj extends JFrame{
     private JPanel panel1;
     private JButton searchB;
     private JButton topB;
+    private JRadioButton shakeB;
+    private JRadioButton hugoB;
+    private JRadioButton tolsB;
+
+    public term_proj() {
+        ButtonGroup buttons = new ButtonGroup();
+        buttons.add(shakeB);
+        buttons.add(hugoB);
+        buttons.add(tolsB);
+        hugoB.setSelected(true);
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("term_proj");
